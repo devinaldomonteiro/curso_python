246beccafc904919bc9631460e0bcc87f59a740d
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2025-11-11 13:33:49 -0300

    Boa aula formatação de strings

---
+++ aula12.py
@@ -0,0 +1,6 @@
+nome = "Inaldo Monteiro"
+altura = 1.75
+peso = 100
+imc = peso // (altura * altura)
+
+print(imc)
---
+++ aula13.py
@@ -0,0 +1,8 @@
+nome = "Inaldo Monteiro"
+altura = 1.75
+peso = 100
+imc = peso // (altura * altura)
+
+imcs = f'{nome} tem {altura:.1f} de altura'
+
+print(imcs)
