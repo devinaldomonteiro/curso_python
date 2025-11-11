f2db87e7f84f881812e0f19ef493371a16e707a4
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2025-11-11 08:02:16 -0300

    Terceiro Commit

--- aula7.py
+++ aula7.py
@@ -1,3 +1,14 @@
+# Variaveis
+
 nome_completo = "Inaldo Marques Monteiro"
 
 print(nome_completo)
+
+nome = "Inaldo"
+
+idade = 44
+
+maior_de_idade = idade > 18
+
+print ('Nome:', nome,'-', 'Idade:', idade)
+print ('É maior?', maior_de_idade)
---
+++ aula8.py
@@ -0,0 +1,14 @@
+nome = "Inaldo"
+sobrenome = "Monteiro"
+idade = 44
+ano_nascimento = 1981
+maior_de_idade = True
+altura_metros = 1.75
+
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print ("Ano de nascimento:", ano_nascimento)
+print("Maior de Idade:", maior_de_idade)
+print ("Altura em metros:", altura_metros)
